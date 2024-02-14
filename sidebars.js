532908a14a979eb2428083f03bab1d78b9186cd4
chaosinthecrd <tom@tmlabs.co.uk>
2024-02-14 15:26:18 +0000

    adding docs-website as submodule
    Signed-off-by: chaosinthecrd <tom@tmlabs.co.uk>

---
+++ sidebars.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docsSidebar: [
+    {
+      type: 'doc',
+      label: 'About',
+      id: 'docs/README',
+    },
+    {
+      type: 'doc',
+      label: 'New Contributors Guide',
+      id: 'docs/CONTRIBUTING',
+    },
+  ],
+}
+
+module.exports = sidebars
